2a4518bf5a33d3446054e937fc0cd28bb38e113f
dongfeng <1156824432@qq.com>
2024-03-23 16:41:55 +0800

    BI项目后端

---
+++ src/main/java/com/yupi/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Dong Feng
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-02-23 10:45:43
+* @Entity com.yupi.springbootinit.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
